d4db90f148f8fe7d12e3146ecd88c9873ea7f5b2
angiemc98 <molinanatalia@hotmail.com>
2021-11-25 20:02:26 -0500

    Proyecto_4

---
+++ pagina/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import './Componentes/styles.css'
+import './js/scripts'
+import Indexform from './Componentes';
+
+
+
+function App() {
+  return (
+    <Indexform></Indexform>
+  );
+}
+
+export default App;
